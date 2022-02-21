6e1782df576f69803846f68a12b87ace9aa46e54
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-02-21 14:19:36 +0100

    inizializzazione js-pwdgen

---
+++ script.js
@@ -0,0 +1,11 @@
+const firstName= prompt(`Dimmi il tuo Nome`);
+console.log(firstName);
+document.getElementById("my-name") .innerHTML = firstName;
+
+const lastName= prompt(`Dimmi il tuo cognome`);
+console.log(lastName);
+document.getElementById("my-surname") .innerHTML = lastName;
+
+const preferitColor= prompt(`Dimmi il tuo colore preferito`);
+console.log(preferitColor);
+document.getElementById("my-color") .innerHTML = preferitColor;
