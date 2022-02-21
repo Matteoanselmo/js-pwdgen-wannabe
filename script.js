c93f2dc17c5d5cff2d8ee4c58aa9734e62268440
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-02-21 14:22:03 +0100

    lastname

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("my-name") .innerHTML = firstName;
 
 const lastName= prompt(`Dimmi il tuo cognome`);
 console.log(lastName);
-document.getElementById("my-surname") .innerHTML = lastName;
+document.getElementById("my-lastname") .innerHTML = lastName;
 
 const preferitColor= prompt(`Dimmi il tuo colore preferito`);
 console.log(preferitColor);
