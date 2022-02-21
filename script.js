b6773af539b5d7136e7900aba0decfc41e3fb5f3
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-02-21 14:43:50 +0100

    aggiunto css e finalizzazione esercizio

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const firstName= prompt(`Dimmi il tuo Nome`);
 console.log(firstName);
-document.getElementById("my-name") .innerHTML = firstName;
 
 const lastName= prompt(`Dimmi il tuo cognome`);
 console.log(lastName);
-document.getElementById("my-lastname") .innerHTML = lastName;
 
 const preferitColor= prompt(`Dimmi il tuo colore preferito`);
 console.log(preferitColor);
-document.getElementById("my-color") .innerHTML = preferitColor;
+
+document.getElementById("my-password") .innerHTML = ( firstName + lastName + preferitColor + "21" );
